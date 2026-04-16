271028e5ecd9ccdb7c4ac9ca7be757add8c2d862
Raunak-g04 <raunakghosh2005@gmail.com>
2026-04-17 00:07:30 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,16 +1,20 @@
 from dataset_loader import load_data
 from model import build_model
-
 DATASET_PATH = "dataset/"
-
 train_gen, val_gen = load_data(DATASET_PATH)
-
 model = build_model()
-
 history = model.fit(
     train_gen,
     validation_data=val_gen,
     epochs=10
 )
-
 model.save("brain_tumor_model.h5")
+import matplotlib.pyplot as plt
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(['Train','Validation'])
+plt.savefig('outputs/accuracy.png')
+    return model
