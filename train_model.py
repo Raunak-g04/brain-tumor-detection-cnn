a4f4f7050f5a053a8505a90a5dffb9ff5121b770
Raunak-g04 <raunakghosh2005@gmail.com>
2026-04-17 00:12:37 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,6 +1,8 @@
 from dataset_loader import load_data
 from model import build_model
-DATASET_PATH = "dataset/"
+import matplotlib.pyplot as plt
+import os
+DATASET_PATH = "../dataset/" 
 train_gen, val_gen = load_data(DATASET_PATH)
 model = build_model()
 history = model.fit(
@@ -8,13 +10,23 @@ history = model.fit(
     validation_data=val_gen,
     epochs=10
 )
-model.save("brain_tumor_model.h5")
-import matplotlib.pyplot as plt
+os.makedirs("../models", exist_ok=True)
+os.makedirs("../outputs", exist_ok=True)
+model.save("../models/brain_tumor_model.h5")
+plt.figure()
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
 plt.title('Model Accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
-plt.legend(['Train','Validation'])
-plt.savefig('outputs/accuracy.png')
-    return model
+plt.legend(['Train', 'Validation'])
+plt.savefig('../outputs/accuracy.png')
+plt.figure()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend(['Train', 'Validation'])
+plt.savefig('../outputs/loss.png')
+print("Training complete. Model and graphs saved.")
