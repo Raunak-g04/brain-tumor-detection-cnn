4d68bdba701bc32fb92258bf8b664805c0376710
Raunak-g04 <raunakghosh2005@gmail.com>
2026-04-17 00:25:08 +0530

    Update dataset_loader.py

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,37 +1,30 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-IMG_SIZE = (160,160)
+import os
+IMG_SIZE = (160, 160)
 BATCH_SIZE = 32
-
 def load_data(dataset_path):
-
+    train_path = os.path.join(dataset_path, "Training")
+    val_path = os.path.join(dataset_path, "Testing")
     train_datagen = ImageDataGenerator(
         rescale=1./255,
         rotation_range=15,
         zoom_range=0.1,
-        horizontal_flip=True,
-        validation_split=0.1
+        horizontal_flip=True
     )
-
     val_datagen = ImageDataGenerator(
-        rescale=1./255,
-        validation_split=0.1
+        rescale=1./255
     )
-
     train_generator = train_datagen.flow_from_directory(
-        dataset_path,
+        train_path,
         target_size=IMG_SIZE,
         batch_size=BATCH_SIZE,
-        class_mode='binary',
-        subset='training'
+        class_mode='binary'
     )
-
     val_generator = val_datagen.flow_from_directory(
-        dataset_path,
+        val_path,
         target_size=IMG_SIZE,
         batch_size=BATCH_SIZE,
-        class_mode='binary',
-        subset='validation'
+        class_mode='binary'
     )
 
     return train_generator, val_generator
