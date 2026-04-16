a5be66f4300ad09d5fed09ef9c1c9b92af6f9b9e
Raunak-g04 <raunakghosh2005@gmail.com>
2026-04-16 13:00:47 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,30 +1,31 @@
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
-
 def build_model():
-
     base_model = MobileNetV2(
         weights="imagenet",
         include_top=False,
         input_shape=(160,160,3)
     )
-
     for layer in base_model.layers:
         layer.trainable = False
-
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
     x = Dense(128, activation="relu")(x)
     x = Dropout(0.5)(x)
     predictions = Dense(1, activation="sigmoid")(x)
-
     model = Model(inputs=base_model.input, outputs=predictions)
-
     model.compile(
         optimizer="adam",
         loss="binary_crossentropy",
         metrics=["accuracy"]
     )
-
+import matplotlib.pyplot as plt
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(['Train', 'Validation'])
+plt.savefig('outputs/accuracy.png')
     return model
